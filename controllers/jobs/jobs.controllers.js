95c451e85923616408a5840e6b673c4dcf3a2ad6
Sechaba Mothobi <sechabamothobi1@outlook.com>
2022-02-23 21:59:47 +0200

    update & delete job

--- controllers/jobs/jobs.controllers.js
+++ controllers/jobs/jobs.controllers.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../../utils/errorResponse');
 const asyncHandler = require('../../middleware/async');
 const Job = require('../../models/job.model');
+const Order = require('../../models/order.model');
 
 // @desc      Get jobs
 // @route     GET /api/v1/jobs
@@ -39,4 +40,111 @@ exports.getJobs = asyncHandler(async (req, res, next) => {
     // } else {
     //     res.status(200).json(res.advancedResults);
     // }
+});
+
+
+// @desc      Get single job
+// @route     GET /api/v1/jobs/:id
+// @access    Public
+exports.getJob = asyncHandler(async (req, res, next) => {
+    const job = await Job.findById(req.params.id).populate({
+        path: 'order',
+        select: 'name pickUpAddress dropOffAddress'
+    });
+
+    if (!job) {
+        return next(
+            new ErrorResponse(`No job with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    res.status(200).json({
+        success: true,
+        data: job
+    });
+});
+
+
+
+// @desc      Add job
+// @route     POST /api/v1/orders/:orderId/jobs
+// @access    Private
+exports.addJob = asyncHandler(async (req, res, next) => {
+    req.body.order = req.params.orderId;
+    // req.body.user = req.user.id;
+
+    const order = await Order.findById(req.params.orderId);
+
+    if (!order) {
+        return next(
+            new ErrorResponse(
+                `No Order with the id of ${req.params.orderId}`,
+                404
+            )
+        );
+    }
+
+    // Make sure user is bootcamp owner
+    // if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    //     return next(
+    //         new ErrorResponse(
+    //             `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+    //             401
+    //         )
+    //     );
+    // }
+
+    const job = await Job.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: job
+    });
+});
+
+
+
+// @desc      Update job
+// @route     PUT /api/v1/jobs/:id
+// @access    Private
+exports.updateJob = asyncHandler(async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {
+        return next(
+            new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        data: job
+    });
+});
+
+
+
+// @desc      Delete job
+// @route     DELETE /api/v1/jobs/:id
+// @access    Private
+exports.deleteJob = asyncHandler(async (req, res, next) => {
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+        return next(
+            new ErrorResponse(`No job with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    await job.remove()
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 });
--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const {
-    getJobs
+    getJobs,
+    getJob,
+    addJob,
+    updateJob,
+    deleteJob
 } = require('../controllers/jobs/jobs.controllers');
 
 const router = express.Router({mergeParams: true});
 
-router.route('/').get(getJobs)
+router
+    .route('/')
+    .get(getJobs)
+    .post(addJob)
+
+router
+    .route('/:id')
+    .get(getJob)
+    .put(updateJob)
+    .delete(deleteJob)
 
 module.exports = router;
